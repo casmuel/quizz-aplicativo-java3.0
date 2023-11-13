8b2169470914011619a05222c6e10054ea65a20c
casmuel <141033916+casmuel@users.noreply.github.com>
2023-11-13 10:33:33 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,76 +1,72 @@
-// creating an array and passing the number, questions, options, and
-answers
+// creating an array and passing the number, questions, options, and answers
 let questions = [
-{
-numb: 1,
-question: "quem nao gostava de Judeu?",
-answer: "Adolf Hitler",
-options: [
-"Albert Einstein",
-"Adolf Hitler",
-"Stephen Hawking",
-"Linus Pauling"
-]
-},
-{
-numb: 2,
-question: "Qual o país mais grande do mundo?",
-answer: "China",
-options: [
-"Timor Lestet",
-"China",
-"Sudão do Sul",
-"Kosovo"
-]
-},
-{
-numb: 3,
-question: "em qual estaçao que a terra fic mais longe do sol?",
-answer: "inverno",
-options: [
-"outono",
-"inverno",
-"verão",
-"outono"
-]
-},
-{
-numb: 4,
-question: "Quantos ossos um ser humano, normalmente tem,
-possui?",
-answer: "206",
-options: [
-"306",
-"206",
-"1400",
-"167"
-]
+  {
+    numb: 1,
+    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
+    answer: "Marie Curiee",
+    options: [
+      "Albert Einstein",
+      "Marie Curiee",
+      "Stephen Hawking",
+      "Linus Pauling"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Qual o país mais novo do mundo?",
+    answer: "Sudão do Sul",
+    options: [
+      "Timor Lestet",
+      "Montenegro",
+      "Sudão do Sul",
+      "Kosovo"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Qual o nome do evento astronômico responsável pelo dia mais longo do ano?",
+    answer: "Solstício de verão",
+    options: [
+      "Equinócio de outono",
+      "Solstício de inverno",
+      "Solstício de verão",
+      "Solstício de outono"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Quantos pares de costelas um ser humano, normalmente, possui?",
+    answer: "12",
+    options: [
+      "12",
+      "11",
+      "14",
+      "13"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Quais planetas do sistema solar realizam seu movimento de rotação em sentido horário?",
+    answer: "Vênus e Urano",
+    options: [
+      "Saturno e Júpiter",
+      "Vênus e Urano",
+      "Terra e Marte",
+      "Mercúrio e Saturno"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
-},
-{
-numb: 5,
-question: "Qual eo satelite natural da Terra?",
-answer: "lua",
-options: [
-"lua",
-"europa",
-"sol",
-"mercurio"
-]
-},
-// you can uncomment the below codes and make duplicate as more as you
-want to add question
-// but remember you need to give the numb value serialize like
-1,2,3,5,6,7,8,9.....
-// {
-// numb: 6,
-// question: "Your Question is Here",
-// answer: "Correct answer of the question is here",
-// options: [
-// "Option 1",
-// "option 2",
-// "option 3",
-// "option 4"
-// ]
-// },
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
 ];
